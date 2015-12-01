bd0f06adc67fd4ac29c02bd3f2f8973c0c2e50ac
Ross Nelson <ross.nelson@gmail.com>
2015-12-01 16:50:31 -0600

    pep8 fixes

--- advent2015/puzzle1.py
+++ advent2015/puzzle1.py
@@ -2,11 +2,16 @@
 """
 http://adventofcode.com/day/1
 
-Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
+Santa is trying to deliver presents in a large apartment building, but he
+can't find the right floor - the directions he got are a little confusing.
+He starts on the ground floor (floor 0) and then follows the instructions
+one character at a time.
 
-An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
+An opening parenthesis, (, means he should go up one floor, and a closing
+parenthesis, ), means he should go down one floor.
 
-The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
+The apartment building is very tall, and the basement is very deep; he will
+never find the top or bottom floors.
 
 For example:
 
@@ -25,22 +30,22 @@ import sys
 
 INFILE = 'inputs/input1.txt'
 
+
 def main():
-        with open(INFILE) as f:
-            data = f.read()
+    with open(INFILE) as f:
+        data = f.read()
 
-            floor = 0
-            for character in data:
-                if '(' == character:
-                    floor = floor + 1
-                elif ')' == character:
-                    floor = floor - 1
-                else:
-                    print('error: invalid input: {}'.format(character))
-                    sys.exit(1)
+        floor = 0
+        for character in data:
+            if '(' == character:
+                floor = floor + 1
+            elif ')' == character:
+                floor = floor - 1
+            else:
+                print('error: invalid input: {}'.format(character))
+                sys.exit(1)
 
-            print('{}'.format(floor))
+        print('{}'.format(floor))
 
 if __name__ == '__main__':
-	main()
-
+    main()
