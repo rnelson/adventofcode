1d02bab3112b467457c7308e4b65b7fc12c145a4
Ross Nelson <ross.nelson@gmail.com>
2017-12-02 11:07:41 -0600

    Day 2a

---
+++ advent2017/src/day02.fs
@@ -0,0 +1,30 @@
+open System
+open System.IO
+open System.Linq
+open System.Text.RegularExpressions
+
+let readInput(filename:string) =
+    let lines = File.ReadAllLines filename
+    [|
+        for l in (lines |> Array.toSeq) do
+            yield Regex.Split(l, "\s+") |> Array.map int
+    |]
+let getDifferencCount arr =
+    let values = [|
+        for row in arr do
+            let minValue = Array.min row
+            let maxValue = Array.max row
+
+            yield maxValue - minValue
+    |]
+    let sum = values |> Seq.sum
+    sum
+
+// Read the input file
+let input = readInput("../inputs/day02.txt")
+
+let part1 = getDifferencCount(input)
+let part2 = 0
+
+printfn "Part 1: %i\nPart 2: %i" part1 part2
+0 |> ignore
