0a62211ecbf1f59055b53445c8fa02e0fe10c803
Ross Nelson <ross.nelson@gmail.com>
2018-12-01 18:56:15 -0600

    More initial framework stuff

---
+++ advent2018/src/main/kotlin/com/github/rnelson/adventofcode/days/Day01.kt
@@ -0,0 +1,13 @@
+package com.github.rnelson.adventofcode.days
+
+import com.github.rnelson.adventofcode.Day
+
+class Day01: Day() {
+    override fun solveA() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun solveB() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
