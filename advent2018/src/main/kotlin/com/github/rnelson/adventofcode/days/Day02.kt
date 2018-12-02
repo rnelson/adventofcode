9b5485d44c5b90e360d0d00c70feee631535d72c
Ross Nelson <ross.nelson@gmail.com>
2018-12-02 10:09:12 -0600

    Add input and shell for day 2

---
+++ advent2018/src/main/kotlin/com/github/rnelson/adventofcode/days/Day02.kt
@@ -0,0 +1,17 @@
+package com.github.rnelson.adventofcode.days
+
+import com.github.rnelson.adventofcode.Day
+
+class Day02: Day() {
+    init {
+        super.setup("02")
+    }
+
+    override fun solveA(): String {
+        return ""
+    }
+
+    override fun solveB(): String {
+        return ""
+    }
+}
