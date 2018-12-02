deca6e31cd3ee82f32d8ea679b3dfd6a1fbab6ba
Ross Nelson <ross.nelson@gmail.com>
2018-12-01 20:25:29 -0600

    Framework work

--- advent2018/src/main/kotlin/com/github/rnelson/adventofcode/days/Day01.kt
+++ advent2018/src/main/kotlin/com/github/rnelson/adventofcode/days/Day01.kt
@@ -3,11 +3,15 @@ package com.github.rnelson.adventofcode.days
 import com.github.rnelson.adventofcode.Day
 
 class Day01: Day() {
-    override fun solveA() {
+    init {
+        super.setup("01")
+    }
+
+    override fun solveA(): String {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun solveB() {
+    override fun solveB(): String {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
