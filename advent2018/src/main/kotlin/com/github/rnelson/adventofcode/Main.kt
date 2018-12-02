5b3c0a832f3a63135909f6d4dbf108296888a22c
Ross Nelson <ross.nelson@gmail.com>
2018-12-01 20:23:31 -0600

    Fix reflection

--- advent2018/src/main/kotlin/com/github/rnelson/adventofcode/Main.kt
+++ advent2018/src/main/kotlin/com/github/rnelson/adventofcode/Main.kt
@@ -1,6 +1,6 @@
 package com.github.rnelson.adventofcode
 
-import com.github.rnelson.adventofcode.days.Day01
+import kotlin.reflect.full.createInstance
 
 fun main(args: Array<String>) {
     if (args.count() != 1) {
@@ -8,11 +8,9 @@ fun main(args: Array<String>) {
         System.exit(1)
     }
 
-    // Create the appropriate "Day" subclass
-    // Pass args[0] to constructor
-    // Call solveA(), solveB() and print the responses
+    val className = "com.github.rnelson.adventofcode.days.Day${args[0]}"
+    val day = Class.forName(className).kotlin.createInstance() as Day
 
-    val day : Day01 = Day01()
     day.setup(args[0])
     day.solveA()
     day.solveB()
