ada7e3c75a106e79be34e68fddf43d76f9e07a37
Ross Nelson <ross.nelson@gmail.com>
2018-12-03 21:15:59 -0600

    Parse input lines into objects

--- advent2018/src/main/kotlin/com/github/rnelson/adventofcode/days/Day03.kt
+++ advent2018/src/main/kotlin/com/github/rnelson/adventofcode/days/Day03.kt
@@ -3,15 +3,50 @@ package com.github.rnelson.adventofcode.days
 import com.github.rnelson.adventofcode.Day
 
 class Day03: Day() {
+    var lines: MutableList<Line> = mutableListOf()
+
     init {
         super.setup("03")
+
+        input.forEach {
+            lines.add(Line.parse(it))
+        }
     }
 
     override fun solveA(): String {
-        return ""
+        return "<unknown>"
     }
 
     override fun solveB(): String {
-        return ""
+        return "<unknown>"
+    }
+
+    class Line {
+        var id: Int = -1
+        var startX: Int = -1
+        var startY: Int = -1
+        var spanX: Int = -1
+        var spanY: Int = -1
+
+        override fun toString(): String {
+            return "#$id @ $startX,$startY: ${spanX}x${spanY}"
+        }
+
+        companion object {
+            fun parse(str: String): Line {
+                val regex = """#(\d+) @ (\d+),(\d+): (\d+)x(\d+)""".toRegex()
+                val result = regex.find(str)
+                val (id, startX, startY, spanX, spanY) = result!!.destructured
+
+                val line = Line()
+                line.id = id.toInt()
+                line.startX = startX.toInt()
+                line.startY = startY.toInt()
+                line.spanX = spanX.toInt()
+                line.spanY = spanY.toInt()
+
+                return line
+            }
+        }
     }
 }
