4c5eefb0eed0cc28b554498e8a4c4f731c713645
Ross Nelson <ross.nelson@gmail.com>
2018-12-09 08:12:27 -0600

    Shell

---
+++ advent2018/src/main/kotlin/com/github/rnelson/adventofcode/days/Day09.kt
@@ -0,0 +1,17 @@
+package com.github.rnelson.adventofcode.days
+
+import com.github.rnelson.adventofcode.Day
+
+class Day09: Day() {
+    init {
+        super.setup("09")
+    }
+
+    override fun solveA(): String {
+        return ""
+    }
+
+    override fun solveB(): String {
+        return ""
+    }
+}
