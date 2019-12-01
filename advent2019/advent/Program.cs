0baf47243180a61036dac93ee1673abe27da013b
Ross Nelson <ross.nelson@gmail.com>
2019-12-01 22:51:40 +0000

    Add day 1

---
+++ advent2019/advent/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+
+[assembly: NeutralResourcesLanguage("en")]
+namespace advent
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var culture = CultureInfo.GetCultureInfo("en-US");
+            var resources = Strings.ResourceManager;
+
+            if (args is null || args.Length != 1)
+                throw new InvalidOperationException(resources.GetString("Help.Usage", CultureInfo.InvariantCulture));
+
+            var number = int.Parse(args[0], culture.NumberFormat);
+            var day = CreateDay(number);
+
+            day?.Header();
+            day?.Part1();
+            day?.Part2();
+        }
+
+        private static Day CreateDay(int dayNumber)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var type = assembly.GetTypes().FirstOrDefault(t => t.Name == $"Day{dayNumber}");
+
+            if (type is null)
+            {
+                Console.Error.WriteLine($"error: unknown day {dayNumber}");
+                return null;
+            }
+
+            var day = (Day) Activator.CreateInstance(type);
+            day.DayNumber = dayNumber;
+
+            return day;
+        }
+    }
+}
---
+++ advent2019/advent/Solutions/Day1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace advent.Solutions
+{
+    [UsedImplicitly]
+    internal class Day1 : Day
+    {
+        private readonly CultureInfo culture = CultureInfo.GetCultureInfo("en-US");
+
+        #region IDay Members
+        protected override ICollection<string> DoPart1()
+        {
+            var modules = Data.Select(int.Parse).ToList();
+            var total = modules.Sum(DetermineFuel);
+
+            return new List<string>() { $"Total fuel: {total}" };
+        }
+
+        protected override ICollection<string> DoPart2()
+        {
+            var modules = Data.Select(int.Parse).ToList();
+            var total = modules.Sum(DetermineFuelRecursive);
+
+            return new List<string>() {$"Total fuel: {total}"};
+        }
+        #endregion IDay Members
+
+        #region Private Methods
+        private int DetermineFuel(int mass)
+        {
+            var a = mass / 3.0;
+            var b = Math.Floor(a);
+            var c = int.Parse(b.ToString(CultureInfo.CurrentCulture), culture.NumberFormat);
+            var d = c - 2;
+
+            return int.Parse(d.ToString(culture), culture.NumberFormat);
+        }
+
+        private int DetermineFuelRecursive(int mass)
+        {
+            var fuel = DetermineFuel(mass);
+            
+            if (fuel < 0)
+                return 0;
+            
+            return fuel + DetermineFuelRecursive(fuel);
+        }
+        #endregion Private Methods
+    }
+}
