545333508306c04b84734ff7ac7432f10f6371ce
Ross Nelson <ross.nelson@gmail.com>
2019-12-02 23:08:12 +0000

    2b

--- advent2019/advent/Solutions/Day2.cs
+++ advent2019/advent/Solutions/Day2.cs
@@ -16,21 +16,30 @@ namespace advent.Solutions
         }
         #endregion Opcodes
 
+        #region Special Positions
+        private enum Position
+        {
+            Noun = 1,
+            Verb = 2
+        }
+        #endregion Special Positions
+
         private int[] Intcode;
+        private const int Output = 19690720;
 
         public Day2()
         {
             DayNumber = 2;
             LoadCommaSeparatedInput();
-
-            Intcode = Data.Select(int.Parse).ToArray();
         }
 
         #region IDay Members
         protected override ICollection<string> DoPart1()
         {
-            Intcode[1] = 12;
-            Intcode[2] = 2;
+            Intcode = Data.Select(int.Parse).ToArray();
+            Intcode[(int)Position.Noun] = 12;
+            Intcode[(int)Position.Verb] = 2;
+
             var ip = 0;
 
             while (Intcode[ip] != (int)Opcode.Halt)
@@ -48,12 +57,48 @@ namespace advent.Solutions
                 }
             }
 
-            return new List<string> {Intcode[0].ToString()};
+            return new List<string> {Intcode[0].ToString(Culture.NumberFormat)};
         }
 
         protected override ICollection<string> DoPart2()
         {
-            return new List<string>();
+            var nouns = Enumerable.Range(0, 100);
+            var verbs = Enumerable.Range(0, 100);
+
+            foreach (var noun in nouns)
+            {
+                foreach (var verb in verbs)
+                {
+                    Intcode = Data.Select(int.Parse).ToArray();
+                    Intcode[(int)Position.Noun] = noun;
+                    Intcode[(int)Position.Verb] = verb;
+
+                    var ip = 0;
+
+                    while (Intcode[ip] != (int)Opcode.Halt)
+                    {
+                        switch (Intcode[ip])
+                        {
+                            case (int)Opcode.Add:
+                                Add(ref ip);
+                                break;
+                            case (int)Opcode.Multiply:
+                                Multiply(ref ip);
+                                break;
+                            case (int)Opcode.Halt:
+                                break;
+                        }
+                    }
+
+                    if (Intcode[0] == Output)
+                    {
+                        var answer = 100 * Intcode[(int)Position.Noun] + Intcode[(int)Position.Verb];
+                        return new List<string> { answer.ToString(Culture.NumberFormat) };
+                    }
+                }
+            }
+
+            return new List<string> {"Unable to find answer"};
         }
         #endregion IDay Members
 
