0d5b94e0e0a566e74ef61366121f7cf4e306f433
Ross Nelson <ross.nelson@gmail.com>
2019-12-03 06:48:02 -0600

    Get ready to do day 3

---
+++ advent2019/advent/Solutions/Day3.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace advent.Solutions
+{
+    [UsedImplicitly]
+    internal class Day3 : Day
+    {
+        public Day3()
+        {
+            DayNumber = 3;
+            LoadInput();
+        }
+
+        #region IDay Members
+        protected override ICollection<string> DoPart1()
+        {
+            return new List<string> { "Not completed" };
+        }
+
+        protected override ICollection<string> DoPart2()
+        {
+            return new List<string> { "Not completed" };
+        }
+        #endregion IDay Members
+
+        #region Private Methods
+        #endregion Private Methods
+    }
+}
