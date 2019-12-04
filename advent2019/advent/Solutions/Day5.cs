fb5dde1a57c37dfb455c43b171e7d3db39bdabed
Ross Nelson <ross.nelson@gmail.com>
2019-12-04 23:43:02 +0000

    Get a head start on day 5

---
+++ advent2019/advent/Solutions/Day5.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace advent.Solutions
+{
+    [UsedImplicitly]
+    internal class Day5 : Day
+    {
+        public Day5()
+        {
+            DayNumber = 5;
+        }
+
+        #region IDay Members
+        protected override ICollection<string> DoPart1()
+        {
+            LoadInput();
+            return new List<string> { "Not yet completed" };
+        }
+
+        protected override ICollection<string> DoPart2()
+        {
+            LoadInput();
+            return new List<string> { "Not yet completed" };
+        }
+        #endregion IDay Members
+
+        #region Private Methods
+        #endregion Private Methods
+    }
+}
