3172a10f504be6d35eac6809b1343e4bf6ee9a64
Ross Nelson <ross.nelson@gmail.com>
2019-12-05 22:24:23 +0000

    5a

--- advent2019/advent/Day.cs
+++ advent2019/advent/Day.cs
@@ -11,8 +11,6 @@ namespace advent
     internal abstract class Day : IDay
     {
         #region Fields
-        private readonly CultureInfo culture = CultureInfo.GetCultureInfo("en-US");
-        private readonly ResourceManager resources = Strings.ResourceManager;
         private int? day;
         #endregion Fields
 
@@ -23,13 +21,14 @@ namespace advent
             set
             {
                 if (value < 1 || value > 31)
-                    throw new ArgumentOutOfRangeException(nameof(value), resources.GetString("Help.InvalidDay", culture));
+                    throw new ArgumentOutOfRangeException(nameof(value), Resources.GetString("Help.InvalidDay", Culture));
 
                 day = value;
             }
         }
 
         protected readonly CultureInfo Culture = CultureInfo.GetCultureInfo("en-US");
+        protected readonly ResourceManager Resources = Strings.ResourceManager;
 
         protected ICollection<string> Data { get; set; } = new List<string>();
 
@@ -40,32 +39,32 @@ namespace advent
         #region Public Methods
         public void Header()
         {
-            var text = resources
-                .GetString("Day.Header", culture)
+            var text = Resources
+                .GetString("Day.Header", Culture)
                 .Replace("\\n", "\n", StringComparison.InvariantCulture);
             Console.WriteLine(text);
         }
 
         public void Part1()
         {
-            Console.WriteLine(string.Format(culture, resources.GetString("Day.Part", culture), "1"));
+            Console.WriteLine(string.Format(Culture, Resources.GetString("Day.Part", Culture), "1"));
             
             var output = DoPart1();
             foreach (var line in output)
             {
-                Console.WriteLine(string.Format(culture, resources.GetString("Day.Part.Line", culture), line));
+                Console.WriteLine(string.Format(Culture, Resources.GetString("Day.Part.Line", Culture), line));
             }
         }
 
         public void Part2()
         {
             Console.WriteLine();
-            Console.WriteLine(string.Format(culture, resources.GetString("Day.Part", culture), "2"));
+            Console.WriteLine(string.Format(Culture, Resources.GetString("Day.Part", Culture), "2"));
 
             var output = DoPart2();
             foreach (var line in output)
             {
-                Console.WriteLine(string.Format(culture, resources.GetString("Day.Part.Line", culture), line));
+                Console.WriteLine(string.Format(Culture, Resources.GetString("Day.Part.Line", Culture), line));
             }
         }
         #endregion Public Methods
@@ -74,7 +73,7 @@ namespace advent
         protected void LoadInput()
         {
             if (!day.HasValue)
-                throw new InvalidOperationException(resources.GetString("Help.SpecifyDay", culture));
+                throw new InvalidOperationException(Resources.GetString("Help.SpecifyDay", Culture));
 
 			var pwd = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             var filename = Path.Combine(pwd, $"../../../Inputs/input{DayNumber}.txt");
