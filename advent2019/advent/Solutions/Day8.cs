b87915aed6eda91b41600befbd105713938878e2
Ross Nelson <ross.nelson@gmail.com>
2019-12-08 14:00:37 +0000

    shell

---
+++ advent2019/advent/Solutions/Day8.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace advent.Solutions
+{
+    [UsedImplicitly]
+    internal class Day8 : Day
+    {
+        public Day8()
+        {
+            DayNumber = 8;
+        }
+
+        #region IDay Members
+        protected override ICollection<string> DoPart1()
+        {
+            LoadInput();
+
+            return new List<string> { "" };
+        }
+
+        protected override ICollection<string> DoPart2()
+        {
+            LoadInput();
+
+            return new List<string> { "" };
+        }
+        #endregion IDay Members
+
+        #region Private Methods
+        #endregion Private Methods
+    }
+}
