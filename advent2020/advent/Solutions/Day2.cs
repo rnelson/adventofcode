0d85f93f5e88b89d759db18e9810df2ea2977b8c
Ross Nelson <ross.nelson@gmail.com>
2020-12-01 08:58:49 -0600

    Add a shell for day 2

---
+++ advent2020/advent/Solutions/Day2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using advent.Exceptions;
+using JetBrains.Annotations;
+
+namespace advent.Solutions
+{
+    [UsedImplicitly]
+    [SuppressMessage("ReSharper", "HeapView.BoxingAllocation")]
+    internal class Day2 : Day
+    {
+        public Day2() : base(2)
+        {
+            LoadInput();
+        }
+
+        #region IDay Members
+        [UsedImplicitly]
+        public override bool Test()
+        {
+            return false;
+        }
+        
+        protected override IEnumerable<string> DoPart1()
+        {
+            var answer = Solve(DataAsInts);
+            return new List<string> {$"[bold yellow]{answer}[/]"};
+        }
+
+        protected override IEnumerable<string> DoPart2()
+        {
+            var answer = Solve(DataAsInts);
+            return new List<string> {$"[bold yellow]{answer}[/]"};
+        }
+        #endregion IDay Members
+
+        #region Private Methods
+        [SuppressMessage("ReSharper", "HeapView.ObjectAllocation.Possible")]
+        private static long Solve(IEnumerable<int> input)
+        {
+            throw new AnswerNotFoundException();
+        }
+        #endregion Private Methods
+    }
+}
