66aca80759990209e195db90e6ec15f869f1b216
Ross Nelson <ross.nelson@gmail.com>
2020-12-01 08:43:29 -0600

    Static analysis fixes

--- advent2020/advent/Exceptions/AnswerNotFoundException.cs
+++ advent2020/advent/Exceptions/AnswerNotFoundException.cs
@@ -6,16 +6,19 @@ namespace advent.Exceptions
     public class AnswerNotFoundException : Exception
     {
         /// <inheritdoc />
+        [UsedImplicitly]
         public AnswerNotFoundException() : base(@"the answer could not be found")
         {
         }
 
         /// <inheritdoc />
+        [UsedImplicitly]
         public AnswerNotFoundException([CanBeNull] string? message) : base(message)
         {
         }
 
         /// <inheritdoc />
+        [UsedImplicitly]
         public AnswerNotFoundException([CanBeNull] string? message, [CanBeNull] Exception? innerException) : base(message, innerException)
         {
         }
