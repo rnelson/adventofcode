a7cdd9b138fc68d8022d655555c4c6e0c1a499ff
Ross Nelson <ross.nelson@gmail.com>
2020-12-01 08:31:40 -0600

    Part A

---
+++ advent2020/advent/Exceptions/AnswerNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace advent.Exceptions
+{
+    public class AnswerNotFoundException : Exception
+    {
+        /// <inheritdoc />
+        public AnswerNotFoundException() : base(@"the answer could not be found")
+        {
+        }
+
+        /// <inheritdoc />
+        public AnswerNotFoundException([CanBeNull] string? message) : base(message)
+        {
+        }
+
+        /// <inheritdoc />
+        public AnswerNotFoundException([CanBeNull] string? message, [CanBeNull] Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
