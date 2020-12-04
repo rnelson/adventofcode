8b8b87d2f4331f912ad85721d73a08113d8e15b3
Ross Nelson <ross.nelson@gmail.com>
2020-12-04 11:03:02 -0600

    Get ready to do day 4

---
+++ advent2020/advent/Solutions/Day4.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+
+namespace advent.Solutions
+{
+    [UsedImplicitly]
+    [SuppressMessage("ReSharper", "HeapView.BoxingAllocation")]
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    internal class Day4 : Day
+    {
+        public Day4() : base(4)
+        {
+            LoadInput();
+        }
+
+        #region IDay Members
+        [UsedImplicitly]
+        [SuppressMessage("ReSharper", "StringLiteralTypo")]
+        public override bool Test()
+        {
+            return false;
+        }
+        
+        protected override IEnumerable<string> DoPartA()
+        {
+            var answer = 0; //Solve(Process(Data), 3, 1);
+            return new List<string> {$"[bold yellow]{answer}[/] valid passports"};
+        }
+
+        protected override IEnumerable<string> DoPartB()
+        {
+            var answer = 0; //slopes.Aggregate(1, (a, b) => a * b);
+            return new List<string> {$"[bold yellow]{answer}[/] valid passports"};
+        }
+        #endregion IDay Members
+
+        #region Private Methods
+        [SuppressMessage("ReSharper", "HeapView.ObjectAllocation.Possible")]
+        [SuppressMessage("ReSharper", "HeapView.ClosureAllocation")]
+        private static int Solve()
+        {
+            return 0;
+        }
+        #endregion Private Methods
+    }
+}
