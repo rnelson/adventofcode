863957dfa149c3d48e25377ccb2777d28ac85fa0
Ross Nelson <ross.nelson@gmail.com>
2020-12-07 10:00:44 -0600

    Get ready for day 8

---
+++ advent2020/advent/Solutions/Day8.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+
+namespace advent.Solutions
+{
+    [UsedImplicitly]
+    [SuppressMessage("ReSharper", "HeapView.BoxingAllocation")]
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    [SuppressMessage("ReSharper", "HeapView.ObjectAllocation.Possible")]
+    [SuppressMessage("ReSharper", "CA1307")]
+    [SuppressMessage("ReSharper", "HeapView.ClosureAllocation")]
+    internal class Day8 : Day
+    {
+        public Day8() : base(8)
+        {
+            LoadInput();
+        }
+
+        #region IDay Members
+        [UsedImplicitly]
+        [SuppressMessage("ReSharper", "StringLiteralTypo")]
+        public override bool Test()
+        {
+            #region Test data
+            var text = new List<string>
+            {
+            };
+            #endregion Test data
+
+            return false;
+        }
+        
+        protected override IEnumerable<string> DoPartA()
+        {
+            var answer = 0; //Solve(Data);
+            return new List<string> {$"[bold yellow]{answer}[/]"};
+        }
+
+        protected override IEnumerable<string> DoPartB()
+        {
+            var answer = 0; //Solve(Data);
+            return new List<string> {$"[bold yellow]{answer}[/]"};
+        }
+        #endregion IDay Members
+
+        #region Private Methods
+        [SuppressMessage("ReSharper", "HeapView.ObjectAllocation.Possible")]
+        [SuppressMessage("ReSharper", "HeapView.ClosureAllocation")]
+        private static int Solve(IEnumerable<string> data)
+        {
+            return 0;
+        }
+        #endregion Private Methods
+    }
+}
