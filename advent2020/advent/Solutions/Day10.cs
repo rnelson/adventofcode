fbe7c7e9723e31353eb91acc4d502d9b70407934
Ross Nelson <ross.nelson@gmail.com>
2020-12-09 15:59:32 -0600

    Prep for day 10

---
+++ advent2020/advent/Solutions/Day10.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using advent.Exceptions;
+using JetBrains.Annotations;
+
+namespace advent.Solutions
+{
+    [UsedImplicitly]
+    [SuppressMessage("ReSharper", "HeapView.BoxingAllocation")]
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    [SuppressMessage("ReSharper", "HeapView.ObjectAllocation.Possible")]
+    internal class Day10 : Day
+    {
+        public Day10() : base(10)
+        {
+            LoadInput();
+        }
+
+        #region IDay Members
+        [UsedImplicitly]
+        [SuppressMessage("ReSharper", "StringLiteralTypo")]
+        public override bool Test()
+        {
+            #region Test data
+            var text = new List<string>
+            {
+            };
+            #endregion Test data
+
+            return false;
+        }
+        
+        protected override IEnumerable<string> DoPartA()
+        {
+            var answer = 0;
+            return new List<string> {$"[bold yellow]{answer}[/]"};
+        }
+
+        protected override IEnumerable<string> DoPartB()
+        {
+            var answer = 0;
+            return new List<string> {$"[bold yellow]{answer}[/]"};
+        }
+        #endregion IDay Members
+
+        #region Private Methods
+        [SuppressMessage("ReSharper", "HeapView.ObjectAllocation.Possible")]
+        [SuppressMessage("ReSharper", "HeapView.ClosureAllocation")]
+        private static long Solve(IEnumerable<string> data)
+        {
+            throw new AnswerNotFoundException();
+        }
+        #endregion Private Methods
+    }
+}
