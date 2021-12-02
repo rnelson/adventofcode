aa482d287136d99ea808a1cc1323b2841f24d3c5
Ross Nelson <ross.nelson@fiserv.com>
2021-12-02 08:50:18 -0600

    Day 2A

---
+++ advent2021/advent.Tests/TestDay02.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using advent.Solutions;
+using advent.Util.Collections;
+
+namespace advent.Tests
+{
+    [TestClass]
+    [SuppressMessage("ReSharper", "HeapView.BoxingAllocation")]
+    public class TestDay02
+    {
+        private readonly string[] input = { "forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2" };
+
+        [TestMethod]
+        public void TestPartA()
+        {
+            const long answerA = 150;
+
+            var day = new Day02(input);
+            var a = (long)day.PartA();
+
+            Assert.AreEqual(answerA, a);
+        }
+
+        [TestMethod]
+        public void TestPartB()
+        {
+            //const long answerB = 5;
+            //
+            //var day = new Day02(input);
+            //var b = (long)day.PartB();
+            //
+            //Assert.AreEqual(answerB, b);
+        }
+    }
+}
---
+++ advent2021/advent.Util/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace advent.Util.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Capitalize(this string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return s;
+
+            return s.Length == 1 ? s.ToUpper() : $"{char.ToUpper(s[0])}{s.Substring(1)}";
+        }
+    }
+}
