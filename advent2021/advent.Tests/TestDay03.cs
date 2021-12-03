6714c4fe22ca423664d434d70c1588d2f52c76d2
Ross Nelson <ross.nelson@fiserv.com>
2021-12-03 11:17:18 -0600

    Day 3

---
+++ advent2021/advent.Tests/TestDay03.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using advent.Solutions;
+
+namespace advent.Tests
+{
+    [TestClass]
+    [SuppressMessage("ReSharper", "HeapView.BoxingAllocation")]
+    public class TestDay03
+    {
+        private readonly string[] input = { "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010" };
+
+        [TestMethod]
+        public void TestPartA()
+        {
+            const long answerA = 198;
+
+            var day = new Day03(input);
+            var a = (long)day.PartA();
+
+            Assert.AreEqual(answerA, a);
+        }
+
+        [TestMethod]
+        public void TestPartB()
+        {
+            const long answerB = 230;
+
+            var day = new Day03(input);
+            var b = (long)day.PartB();
+
+            Assert.AreEqual(answerB, b);
+        }
+    }
+}
---
+++ advent2021/advent.Util/Extensions/ListExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace advent.Util.Extensions
+{
+    public static class ListExtensions
+    {
+        public static IEnumerable<object> Duplicate(this IEnumerable<object> values) => values.Select(o => o).ToList();
+
+        public static IList<object> DuplicateList(this IEnumerable<object> values) => values.Select(o => o).ToList();
+
+        public static List<string> DumbCopyList(this IEnumerable<string> values) => values.ToList();
+    }
+}
