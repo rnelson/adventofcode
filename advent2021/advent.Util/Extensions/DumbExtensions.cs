1e344c8260c9738581b68d1cf233a2290767c700
Ross Nelson <ross.nelson@gmail.com>
2021-12-05 12:58:52 -0600

    Day 5

---
+++ advent2021/advent.Util/Extensions/DumbExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace advent.Util.Extensions
+{
+    public static class DumbExtensions
+    {
+        public static long Count(this int[,] array, Func<int, bool> predicate)
+        {
+            if (predicate is null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            var count = 0L;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    if (predicate(array[i, j]))
+                        count++;
+
+            return count;
+        }
+    }
+}
