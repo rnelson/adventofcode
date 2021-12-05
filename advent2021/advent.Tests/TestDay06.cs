e227b4b7de447947fcd90f118775d25866aa3158
Ross Nelson <ross.nelson@gmail.com>
2021-12-05 13:03:50 -0600

    Prep for day 6

---
+++ advent2021/advent.Tests/TestDay06.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using advent.Solutions;
+
+namespace advent.Tests
+{
+    [TestClass]
+    [SuppressMessage("ReSharper", "HeapView.BoxingAllocation")]
+    public class TestDay06
+    {
+        private readonly string[] input = { "" };
+
+        [TestMethod]
+        public void TestPartA()
+        {
+            const long answerA = 0;
+
+            var day = new Day06(input);
+            var a = (long)day.PartA();
+
+            Assert.AreEqual(answerA, a);
+        }
+
+        [TestMethod]
+        public void TestPartB()
+        {
+            const long answerB = 0;
+
+            var day = new Day06(input);
+            var b = (long)day.PartB();
+
+            Assert.AreEqual(answerB, b);
+        }
+    }
+}
---
+++ advent2021/advent/Solutions/Day06.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+
+namespace advent.Solutions
+{
+    [UsedImplicitly]
+    [SuppressMessage("ReSharper", "HeapView.BoxingAllocation")]
+    public class Day06 : Day
+    {
+        #region Constructors
+        /// <inheritdoc/>
+        public Day06() { }
+
+        /// <inheritdoc/>
+        public Day06(IEnumerable<string> data) : base(data) { }
+        #endregion Constructors
+
+        #region Day Members
+        /// <inheritdoc/>
+        public override object PartA()
+        {
+            return string.Empty;
+        }
+
+        /// <inheritdoc/>
+        public override object PartB()
+        {
+            return string.Empty;
+        }
+        #endregion Day Members
+
+        #region Private Methods
+        #endregion Private Methods
+
+        #region Classes
+        #endregion Classes
+    }
+}
