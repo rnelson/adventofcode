a23e63aa72501dff7ffbf473f59146ededd28bc7
Ross Nelson <ross.nelson@gmail.com>
2021-12-07 19:04:52 -0600

    Prep day 8

---
+++ advent2021/advent/Solutions/Day08.cs
@@ -0,0 +1,35 @@
+﻿namespace advent.Solutions
+{
+    [UsedImplicitly]
+    [SuppressMessage("ReSharper", "HeapView.BoxingAllocation")]
+    public class Day08 : Day
+    {
+        #region Constructors
+        /// <inheritdoc/>
+        public Day08() { }
+
+        /// <inheritdoc/>
+        public Day08(IEnumerable<string> data) : base(data) { }
+        #endregion Constructors
+
+        #region Day Members
+        /// <inheritdoc/>
+        public override object PartA()
+        {
+            return string.Empty;
+        }
+
+        /// <inheritdoc/>
+        public override object PartB()
+        {
+            return string.Empty;
+        }
+        #endregion Day Members
+
+        #region Private Methods
+        #endregion Private Methods
+
+        #region Classes
+        #endregion Classes
+    }
+}
