005936376dd2285421bb4b9417fff4e33399d68e
Ross Nelson <ross.nelson@gmail.com>
2021-12-08 01:42:06 -0600

    Day 8a

--- advent2021/advent.Tests/TestDay08.cs
+++ advent2021/advent.Tests/TestDay08.cs
@@ -4,12 +4,12 @@
     [SuppressMessage("ReSharper", "HeapView.BoxingAllocation")]
     public class TestDay08
     {
-        private readonly string[] input = { "" };
+        private readonly string[] input = { "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe", "edbfga begcd cbg gc gcadebf fbgde acbgfdabcde gfcbed gfec | fcgedb cgb dgebacf gc", "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg", "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb", "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegbdgceab fcbdga | gecf egdcabf bgf bfgea", "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb", "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe", "bdfegc cbegaf gecbf dfcage bdacg ed bedf cedadcbefg gebcd | ed bcgafe cdgba cbgef", "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb", "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce" };
 
         [TestMethod]
         public void TestPartA()
         {
-            const long answerA = 0;
+            const long answerA = 26;
 
             var day = new Day08(input);
             var a = (long)day.PartA();
--- advent2021/advent/Solutions/Day08.cs
+++ advent2021/advent/Solutions/Day08.cs
@@ -12,16 +12,60 @@
         public Day08(IEnumerable<string> data) : base(data) { }
         #endregion Constructors
 
+        #region Properties
+        private Dictionary<int, string> Mapping { get; set; } = new()
+        {
+            { 0, "abcefg" },
+            { 1, "cf" },
+            { 2, "acdeg" },
+            { 3, "acdfg" },
+            { 4, "bcdf" },
+            { 5, "abdfg" },
+            { 6, "abdefg" },
+            { 7, "acf" },
+            { 8, "abcdefg" },
+            { 9, "abcdfg" },
+        };
+        #endregion Properties
+
         #region Day Members
         /// <inheritdoc/>
         public override object PartA()
         {
-            return string.Empty;
+            var uniques = new[] { 1, 4, 7, 8 };
+            var counts = new Dictionary<string, long>();
+
+            for (var i = 0; i < 10; i++)
+                counts[i.ToString()] = 0;
+
+            foreach (var line in Data)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                var bits = line.Split(" | ");
+                var input = bits[0].Split(' ');
+                var output = bits[1].Split(' ');
+
+                foreach (var number in output)
+                {
+                    var candidates = Mapping.Where(kvp => kvp.Value.Length == number.Length).ToList();
+                    
+                    if (candidates.Count() != 1) continue;
+                    counts[candidates.First().Key.ToString()]++;
+                }
+            }
+
+            return uniques.Sum(unique => counts[unique.ToString()]);
         }
 
         /// <inheritdoc/>
         public override object PartB()
         {
+            // start with 1, 4, 7, 8 and assume they are in the same order in
+            // input as on the page? bea is my 3 letter (I think), which means
+            // it is 7. b is top top, e is top right, a is bottom right. Use
+            // those to sort out all of the locations?
+
             return string.Empty;
         }
         #endregion Day Members
