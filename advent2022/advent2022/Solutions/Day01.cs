dd9f2aa8e5122bfd1e06c3254db94a550a122618
Ross Nelson <ross.nelson@gmail.com>
2022-11-13 15:43:06 -0600

    Add a framework for 2022/C#

---
+++ advent2022/advent2022/Solutions/Day01.cs
@@ -0,0 +1,16 @@
+﻿namespace advent2022.Solutions;
+
+public class Day01 : DayBase
+{
+    /// <inheritdoc />
+    public override object A(IEnumerable<string> input)
+    {
+        return string.Empty;
+    }
+
+    /// <inheritdoc />
+    public override object B(IEnumerable<string> input)
+    {
+        return string.Empty;
+    }
+}
