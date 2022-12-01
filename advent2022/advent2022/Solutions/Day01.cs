8ec58066277f5c266676c715b4e1322a3ac0c9ad
Ross Nelson <ross.nelson@gmail.com>
2022-12-01 14:34:29 -0600

    Spacing

--- advent2022/advent2022/Solutions/Day01.cs
+++ advent2022/advent2022/Solutions/Day01.cs
@@ -8,10 +8,10 @@ public class Day01 : DayBase
 	public override (object, object) Solve()
 	{
 		var sums = 
-			Input!.GroupByDivider<string>(string.Empty) // group by elf
-			.Select(GetNumericInputs<int>).ToList()     // str -> int
-			.OrderByDescending(n => n.Sum())            // sum each elf's pile
-			.ToArray();                                 // avoid multiple enumerations
+			Input!.GroupByDivider<string>(string.Empty)   // group by elf
+			.Select(GetNumericInputs<int>).ToList()       // str -> int
+			.OrderByDescending(n => n.Sum())              // sum each elf's pile
+			.ToArray();                                   // avoid multiple enumerations
 
 		return (sums.First().Sum(), sums.Take(3).Sum(n => n.Sum()));
 	}
