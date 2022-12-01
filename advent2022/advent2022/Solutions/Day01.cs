0104a2a45dcc1b327a1b3d04ff87c466c4ae7e53
Ross Nelson <ross.nelson@gmail.com>
2022-12-01 00:08:13 -0600

    Day 1

--- advent2022/advent2022/Solutions/Day01.cs
+++ advent2022/advent2022/Solutions/Day01.cs
@@ -1,16 +1,27 @@
-﻿namespace advent2022.Solutions;
+﻿using System.Globalization;
+using advent2022.Share;
+
+namespace advent2022.Solutions;
 
 public class Day01 : DayBase
 {
     /// <inheritdoc />
     public override object A(IEnumerable<string> input)
     {
-        return string.Empty;
+        var groups = input.GroupByDivider<string>(string.Empty);
+        var elfCalorieCounts = groups.Select(GetNumericInputs<int>).ToList();
+        var descendingTotals = elfCalorieCounts.OrderByDescending(n => n.Sum());
+
+        return descendingTotals.First().Sum();
     }
 
     /// <inheritdoc />
     public override object B(IEnumerable<string> input)
     {
-        return string.Empty;
+        var groups = input.GroupByDivider<string>(string.Empty);
+        var elfCalorieCounts = groups.Select(GetNumericInputs<int>).ToList();
+        var descendingTotals = elfCalorieCounts.OrderByDescending(n => n.Sum());
+
+        return descendingTotals.Take(3).Sum(l => l.Sum());
     }
 }
