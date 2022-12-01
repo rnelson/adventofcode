d6edf0eb4ccb52c4661e49eb059f90b0e70ffa0a
Ross Nelson <ross.nelson@gmail.com>
2022-12-01 15:09:38 -0600

    Prettify

--- advent2022/advent2022/Solutions/Day01.cs
+++ advent2022/advent2022/Solutions/Day01.cs
@@ -8,10 +8,10 @@ public class Day01 : DayBase
 	public override (object, object) Solve()
 	{
 		var sums = Input!
-			.GroupByDivider<string>(string.Empty)         // group by elf
-			.Select(GetNumbers<int>)                      // str -> int
-			.OrderByDescending(n => n.Sum())              // sum each elf's pile
-			.ToArray();                                   // avoid multiple enumerations
+			.GroupByDivider<string>(string.Empty)    // group by elf
+			.Select(GetNumbers<int>)                 // str -> int
+			.OrderByDescending(n => n.Sum())         // sum each elf's pile
+			.ToArray();                              // avoid multiple enumerations
 
 		return (sums.First().Sum(), sums.Take(3).Sum(n => n.Sum()));
 	}
