a6c84b3dc26b22f00c8f9445e295f3e48e84c084
Ross Nelson <ross.nelson@gmail.com>
2022-12-02 09:05:44 -0600

    Cleanup

--- advent2022/advent2022/Solutions/Day02.cs
+++ advent2022/advent2022/Solutions/Day02.cs
@@ -12,14 +12,14 @@ public class Day02 : DayBase
 		{
 			var (os, ps) = Parse(line);
 			
-			scoreA += ScoreA(os, ps);
-			scoreB += ScoreB(os, ps);
+			scoreA += Score(os, ps);
+			scoreB += PlayAndScore(os, ps);
 		}
 		
 		return (scoreA, scoreB);
 	}
 
-	private (OpponentShape, PlayerShape) Parse(string input)
+	private static (OpponentShape, PlayerShape) Parse(string input)
 	{
 		var shapes = input.Split(' ');
 		Enum.TryParse(shapes[0], out OpponentShape os);
@@ -28,7 +28,7 @@ public class Day02 : DayBase
 		return (os, ps);
 	}
 
-	private int ScoreA(OpponentShape os, PlayerShape ps)
+	private static int Score(OpponentShape os, PlayerShape ps)
 	{
 		var won = IsWinner(os, ps);
 		var draw = (int)os == (int)ps;
@@ -36,7 +36,7 @@ public class Day02 : DayBase
 		return (won ? 6 : 0) + (draw ? 3 : 0) + (int)ps;
 	}
 
-	private int ScoreB(OpponentShape os, PlayerShape ps)
+	private static int PlayAndScore(OpponentShape os, PlayerShape ps)
 	{
 		var requiredShape = PlayerShape.X;
 
@@ -65,35 +65,19 @@ public class Day02 : DayBase
 			},
 			_ => requiredShape
 		};
-
-		var won = IsWinner(os, requiredShape);
-		var draw = (int)os == (int)requiredShape;
-
-		return (won ? 6 : 0) + (draw ? 3 : 0) + (int)requiredShape;
+		
+		return Score(os, requiredShape);
 	}
 
-	private bool IsWinner(OpponentShape os, PlayerShape ps)
+	private static bool IsWinner(OpponentShape os, PlayerShape ps) => os switch
 	{
-		return os switch
-		{
-			OpponentShape.A => ps == PlayerShape.Y,
-			OpponentShape.B => ps == PlayerShape.Z,
-			OpponentShape.C => ps == PlayerShape.X,
-			_ => false
-		};
-	}
+		OpponentShape.A => ps == PlayerShape.Y,
+		OpponentShape.B => ps == PlayerShape.Z,
+		OpponentShape.C => ps == PlayerShape.X,
+		_ => false
+	};
 
-	private enum OpponentShape
-	{
-		A = 1, // Rock
-		B, // Paper
-		C // Scissors
-	}
-
-	private enum PlayerShape
-	{
-		X = 1, // Rock
-		Y, // Paper
-		Z // Scissors
-	}
+	private enum OpponentShape { A = 1, B, C }
+	
+	private enum PlayerShape { X = 1, Y, Z }
 }
