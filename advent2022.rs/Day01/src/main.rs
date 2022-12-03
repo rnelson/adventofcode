eecd60fbd5ca139ad2726c11f7168ed6b9863c11
Ross Nelson <ross.nelson@gmail.com>
2022-12-03 14:27:40 -0600

    I don't know Rust.

---
+++ advent2022.rs/Day01/src/main.rs
@@ -0,0 +1,27 @@
+use std::fs;
+
+fn main() {
+	println!("AoC 2022 - Day 1: Calorie Counting");
+
+	let input = fs::read_to_string("input.txt").expect("cannot read input file");
+    let lines: Vec<&str> = input.lines().collect();
+    
+    let mut calories: Vec<u32> = vec![];
+    let mut sums: Vec<u32> = vec![];
+
+    for line in lines {
+        println!("[{}]", line);
+
+        if line.is_empty() {
+            // todo: store sum
+            calories = vec![];
+        } else {
+            println!("<{}>", line);
+
+            let number = line.parse::<u32>().expect("could not parse");
+            calories.push(number);
+
+            println!("{}", number);
+        }
+    }
+}
