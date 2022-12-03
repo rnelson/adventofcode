78ed2bc5939539c42711a7b632670b14732ea725
Ross Nelson <ross.nelson@gmail.com>
2022-12-03 02:06:38 -0600

    Be more consistent in naming

--- advent2022/advent2022/Solutions/Day03.cs
+++ advent2022/advent2022/Solutions/Day03.cs
@@ -7,13 +7,13 @@ public class Day03 : DayBase
 	/// <inheritdoc />
 	public override (object, object) Solve() => (
 		Input!.Sum(line => line.Halve().ToTuple().IntersectStringTuple().ScorePriority()),
-		Input!.Chunk(3).Sum(lines => lines.IntersectStrings().ScorePriority())
+		Input!.Chunk(3).Sum(lines => lines.IntersectStringArray().ScorePriority())
 	);
 }
 
-internal static class Day3Extensions
+internal static class Day03Extensions
 {
-	public static string IntersectStrings(this string[] arr)
+	public static string IntersectStringArray(this string[] arr)
 	{
 		switch (arr.Length)
 		{
