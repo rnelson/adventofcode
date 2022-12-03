2a57e2b40de2b00539c1c4a8b8d9c7ff215c65af
Ross Nelson <ross.nelson@gmail.com>
2022-12-03 01:57:25 -0600

    Add the rest of the files for day 5 this time

---
+++ advent2022/advent2022.Test/Day05Test.cs
@@ -0,0 +1,62 @@
+﻿using advent2022.Share;
+using advent2022.Solutions;
+
+namespace advent2022.Test;
+
+[TestClass]
+public class Day05Test
+{
+	private readonly IDay sample = new Day05(); 
+	private readonly IDay real = new Day05();
+	private object? sampleA;
+	private object? sampleB;
+	private object? realA;
+	private object? realB;
+
+	[TestInitialize]
+	public void Setup()
+	{
+		#region Load sample and real input data
+		var sampleInput = new[]
+		{
+			""
+		};
+		sample.Input = sampleInput;
+
+		var realInput = real.LoadInput();
+		real.Input = realInput;
+		#endregion Load sample and real input data
+
+		// Solve all four problems at once
+		(sampleA, sampleB) = sample.Solve();
+		(realA, realB) = real.Solve();
+	}
+    
+	[TestMethod]
+	public void SampleA()
+	{
+		const int solution = 0;
+		Assert.AreEqual(solution, sampleA);
+	}
+    
+	[TestMethod]
+	public void SampleB()
+	{
+		const int solution = 0;
+		Assert.AreEqual(solution, sampleB);
+	}
+    
+	[TestMethod]
+	public void SolutionA()
+	{
+		const int solution = 0;
+		Assert.AreEqual(solution, realA);
+	}
+    
+	[TestMethod]
+	public void SolutionB()
+	{
+		const int solution = 0;
+		Assert.AreEqual(solution, realB);
+	}
+}
