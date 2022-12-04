d6f84e9c6ecf95d9082ff2006374672b499c1a6e
Ross Nelson <ross.nelson@gmail.com>
2022-12-04 15:18:08 -0600

    Reading comprehension is difficult

---
+++ advent2022/advent2022.Share/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace advent2022.Share;
+
+public static class ListExtensions
+{
+	public static List<Tuple<T, T>> Pairs<T>(this List<T> list)
+	{
+		var result = new List<Tuple<T, T>>();
+		
+		for (var i = 0; i < list.Count; i++)
+			for (var j = i + 1; j < list.Count; j++)
+				result.Add(new Tuple<T, T>(list.ElementAt(i), list.ElementAt(j)));
+
+		return result;
+	}
+}
