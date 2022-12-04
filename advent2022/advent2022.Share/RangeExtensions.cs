61322accf880161dfb6dcab7a095a3b671e893c2
Ross Nelson <ross.nelson@gmail.com>
2022-12-04 15:30:57 -0600

    Stop being a dummy

--- advent2022/advent2022.Share/RangeExtensions.cs
+++ advent2022/advent2022.Share/RangeExtensions.cs
@@ -4,16 +4,7 @@ public static class RangeExtensions
 {
 	public static bool Contains(this Range r1, Range r2) => r1.Start.Value <= r2.Start.Value && r1.End.Value >= r2.End.Value;
 
-	public static bool Overlaps(this Range r1, Range r2)
-	{
-		var range1 = new List<int>();
-		for (var i = r1.Start.Value; i <= r1.End.Value; i++)
-			range1.Add(i);
-		
-		var range2 = new List<int>();
-		for (var i = r2.Start.Value; i <= r2.End.Value; i++)
-			range2.Add(i);
-
-		return range1.Intersect(range2).Any();
-	}
+	public static bool Overlaps(this Range r1, Range r2) =>
+		(r1.Start.Value <= r2.End.Value && r1.End.Value >= r2.Start.Value) &&
+		(r2.Start.Value <= r1.End.Value && r2.End.Value >= r1.Start.Value);
 }
