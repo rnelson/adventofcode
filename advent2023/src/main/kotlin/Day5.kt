e1e0cc8c672d92a3640922c9d6fd758d3efdde2c
Ross Nelson <ross.nelson@gmail.com>
2023-12-04 16:02:06 -0600

    Add 2023d5 files

---
+++ advent2023/src/main/kotlin/Day5.kt
@@ -0,0 +1,9 @@
+class Day05 : Day(5) {
+    override fun partA(input: Array<String>): Any {
+        return 0
+    }
+
+    override fun partB(input: Array<String>): Any {
+        return 0
+    }
+}
---
+++ advent2023/src/test/kotlin/Day05Test.kt
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day05Test {
+    @Test
+    fun partA() {
+        val input = arrayOf("")
+        val actual = Day05().partA(input)
+        assertEquals(0, actual)
+    }
+
+    @Test
+    fun partB() {
+        val input = arrayOf("")
+        val actual = Day05().partB(input)
+        assertEquals(0, actual)
+    }
+}
