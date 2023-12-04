3c867661622493300d3931f41585e3660db05305
Ross Nelson <ross.nelson@gmail.com>
2023-12-04 14:21:56 -0600

    add d4 files

---
+++ advent2023/src/main/kotlin/Day04.kt
@@ -0,0 +1,9 @@
+class Day04 : Day(4) {
+    override fun partA(input: Array<String>): Any {
+        return 0
+    }
+
+    override fun partB(input: Array<String>): Any {
+        return 0
+    }
+}
