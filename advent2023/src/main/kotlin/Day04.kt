7cb5db53995714a5e6ff3fed7bf03ad47dacbd58
Ross Nelson <ross.nelson@gmail.com>
2023-12-04 15:34:52 -0600

    committing what I have on d4b before rethinking it

--- advent2023/src/main/kotlin/Day04.kt
+++ advent2023/src/main/kotlin/Day04.kt
@@ -1,14 +1,14 @@
 class Day04 : Day(4) {
     override fun partA(input: Array<String>): Any {
-        val map = score(input)
+        val map = scoreA(input)
         return map.values.reduce { acc, i -> acc + i }
     }
 
     override fun partB(input: Array<String>): Any {
-        return 0
+        return scoreB(input)
     }
 
-    private fun score(input: Array<String>): Map<Int, Int> {
+    private fun scoreA(input: Array<String>, double: Boolean = true): Map<Int, Int> {
         val results = mutableMapOf<Int, Int>()
 
         input.forEach { line ->
@@ -25,7 +25,7 @@ class Day04 : Day(4) {
             winningNumbers.forEach { n ->
                 if (myNumbers.contains(n)) {
                     total = add
-                    add *= 2
+                    add = if (double) add * 2 else add + 1
                 }
             }
 
@@ -33,5 +33,45 @@ class Day04 : Day(4) {
         }
 
         return results;
+    }
+
+    private fun scoreB(input: Array<String>): Int {
+        val scores = scoreA(input, false)
+        val instances = mutableMapOf<Int, Int>()
+
+        println(scores)
+
+        // Initially, we have one copy of each
+        for (i in 1..scores.size) {
+            instances[i] = 1
+        }
+
+        // Now go through our scores and add copies of cards
+        for (i in 1..scores.size) {
+            val points = scores[i] ?: 0
+            println("Card $i got $points points and ${instances[i]} turns [${scores[i]}]")
+
+            if (points > 0) {
+                val copies = instances[i] ?: 0
+                println("Card $i got $points points and $copies turns [${scores[i]}]")
+
+                for (turn in 1..copies) {
+                    println("Turn $turn")
+                    val start = i
+                    val end = i + points
+
+                    for (j in start..<end) {
+                        val newCount = (instances[j] ?: 0) + (instances[i] ?: 0)
+                        instances[j] = newCount
+
+                        println("Added another card ${j + 1} (now $newCount copies)")
+                    }
+                }
+            }
+
+            println("")
+        }
+
+        return instances.values.reduce { acc, i -> acc + i }
     }
 }
