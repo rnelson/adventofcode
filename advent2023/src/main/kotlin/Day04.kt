6d4acf96a4138b6221fbecacaa43ff862b7868b7
Ross Nelson <ross.nelson@gmail.com>
2023-12-04 16:00:18 -0600

    2023 day 4

--- advent2023/src/main/kotlin/Day04.kt
+++ advent2023/src/main/kotlin/Day04.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 class Day04 : Day(4) {
     override fun partA(input: Array<String>): Any {
         val map = scoreA(input)
@@ -8,68 +10,47 @@ class Day04 : Day(4) {
         return scoreB(input)
     }
 
-    private fun scoreA(input: Array<String>, double: Boolean = true): Map<Int, Int> {
+    private fun getMatchCount(input: String): Pair<Int, Int> {
+        val split1 = input.split(":")
+        val split2 = split1[1].split(" | ")
+
+        val gameNumber = split1[0].trim().substring(5).trim().toInt()
+        val myNumbers = split2[0].trim().replace("\\s+".toRegex(), " ").split(" ").map { it.trim().toInt() }
+        val winningNumbers = split2[1].trim().replace("\\s+".toRegex(), " ").split(" ").map { it.trim().toInt() }.toTypedArray()
+
+        return Pair(gameNumber, winningNumbers.intersect(myNumbers.toSet()).size)
+    }
+
+    private fun scoreA(input: Array<String>): Map<Int, Int> {
         val results = mutableMapOf<Int, Int>()
 
         input.forEach { line ->
-            val split1 = line.split(":")
-            val split2 = split1[1].split(" | ")
+            val matchDetails = getMatchCount(line)
+            val gameNumber = matchDetails.first
+            val matches = matchDetails.second
 
-            val gameNumber = split1[0].trim().substring(5).trim().toInt()
-            val myNumbers = split2[0].trim().replace("\\s+".toRegex(), " ").split(" ").map { it.trim().toInt() }
-            val winningNumbers = split2[1].trim().replace("\\s+".toRegex(), " ").split(" ").map { it.trim().toInt() }
-
-            var total = 0
-            var add = 1
-
-            winningNumbers.forEach { n ->
-                if (myNumbers.contains(n)) {
-                    total = add
-                    add = if (double) add * 2 else add + 1
-                }
-            }
-
-            results[gameNumber] = total
+            results[gameNumber] = if (matches > 0) 2.toDouble().pow(matches - 1).toInt() else 0
         }
 
-        return results;
+        return results
     }
 
     private fun scoreB(input: Array<String>): Int {
-        val scores = scoreA(input, false)
+        val scores = scoreA(input)
         val instances = mutableMapOf<Int, Int>()
 
-        println(scores)
-
-        // Initially, we have one copy of each
-        for (i in 1..scores.size) {
+        for (i in 0..<scores.size) {
             instances[i] = 1
         }
 
-        // Now go through our scores and add copies of cards
-        for (i in 1..scores.size) {
-            val points = scores[i] ?: 0
-            println("Card $i got $points points and ${instances[i]} turns [${scores[i]}]")
+        for (i in input.indices) {
+            val line = input[i]
+            val matches = getMatchCount(line).second
 
-            if (points > 0) {
-                val copies = instances[i] ?: 0
-                println("Card $i got $points points and $copies turns [${scores[i]}]")
-
-                for (turn in 1..copies) {
-                    println("Turn $turn")
-                    val start = i
-                    val end = i + points
-
-                    for (j in start..<end) {
-                        val newCount = (instances[j] ?: 0) + (instances[i] ?: 0)
-                        instances[j] = newCount
-
-                        println("Added another card ${j + 1} (now $newCount copies)")
-                    }
-                }
+            for (j in i+1..<(i+1+matches)) {
+                val c = instances[i] ?: 0
+                instances[j] = (instances[j] ?: 0) + c
             }
-
-            println("")
         }
 
         return instances.values.reduce { acc, i -> acc + i }
