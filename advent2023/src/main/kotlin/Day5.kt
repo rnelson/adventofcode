70d67a4ba37ce96590ca378e9d6bcef5655e9888
Ross Nelson <ross.nelson@gmail.com>
2023-12-06 15:49:03 -0600

    add partial d5

--- advent2023/src/main/kotlin/Day5.kt
+++
@@ -1,9 +0,0 @@
-class Day05 : Day(5) {
-    override fun partA(input: Array<String>): Any {
-        return 0
-    }
-
-    override fun partB(input: Array<String>): Any {
-        return 0
-    }
-}
