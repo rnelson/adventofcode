0ce9816e00bc909f60cfb705f7ed3dcd74516aa1
Ross Nelson <ross.nelson@gmail.com>
2023-12-07 14:04:33 -0600

    Add d8 files

---
+++ advent2023/src/main/kotlin/Day08.kt
@@ -0,0 +1,9 @@
+class Day08 : Day(8) {
+    override fun partA(input: Array<String>): Any {
+        return 0
+    }
+
+    override fun partB(input: Array<String>): Any {
+        return 0
+    }
+}
---
+++ advent2023/src/test/kotlin/Day08Test.kt
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day08Test {
+    private val input = arrayOf("")
+
+    @Test
+    fun partA() {
+        val actual = Day08().partA(input)
+        assertEquals(0, actual)
+    }
+
+    @Test
+    fun partB() {
+        val actual = Day08().partB(input)
+        assertEquals(0, actual)
+    }
+}
