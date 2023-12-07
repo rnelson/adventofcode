4e3d29bb08d7402224d6350a1f04002e5aa2dc56
Ross Nelson <ross.nelson@gmail.com>
2023-12-06 18:52:37 -0600

    Add files for day 7

---
+++ advent2023/src/main/kotlin/Day07.kt
@@ -0,0 +1,9 @@
+class Day07 : Day(7) {
+    override fun partA(input: Array<String>): Any {
+        return 0
+    }
+
+    override fun partB(input: Array<String>): Any {
+        return 0
+    }
+}
--- advent2023/src/main/kotlin/Main.kt
+++ advent2023/src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
 fun main(ignoredArgs: Array<String>) {
-    val day = Day05()
+    val day = Day07()
     day.solve()
 }
