012f121559e0b3b833b1c3be26d0cfa5b8da0104
Ross Nelson <ross.nelson@gmail.com>
2023-12-06 18:35:28 -0600

    Simplify timing

--- advent2023/src/main/kotlin/Util.kt
+++ advent2023/src/main/kotlin/Util.kt
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 fun String.getIntegers(): Array<Int> {
     return this
         .trim()
@@ -20,4 +23,25 @@ fun Array<Long>.smoosh(): String {
     var result = ""
     this.forEach { result += it.toString() }
     return result
+}
+
+fun <T>time(block: () -> T) {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    val start = LocalDateTime.now().format(formatter)
+    val startMS = System.currentTimeMillis()
+
+    try {
+        block.invoke()
+    }
+    finally {
+        val end = LocalDateTime.now().format(formatter)
+        val endMS = System.currentTimeMillis()
+        val duration = endMS - startMS
+
+        print("Took $duration ms")
+        if (duration > 1000) {
+            print(" (started $start, finished $end)")
+        }
+        println()
+    }
 }
