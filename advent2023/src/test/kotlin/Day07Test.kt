20dd6dc4084b255190f3dcfbec9021536b5945ba
Ross Nelson <ross.nelson@gmail.com>
2023-12-07 12:43:52 -0600

    7a

--- advent2023/src/test/kotlin/Day07Test.kt
+++ advent2023/src/test/kotlin/Day07Test.kt
@@ -2,12 +2,16 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class Day07Test {
-    private val input = arrayOf("")
+    private val input = arrayOf("32T3K 765",
+            "T55J5 684",
+            "KK677 28",
+            "KTJJT 220",
+            "QQQJA 483")
 
     @Test
     fun partA() {
         val actual = Day07().partA(input)
-        assertEquals(0, actual)
+        assertEquals(6440, actual)
     }
 
     @Test
