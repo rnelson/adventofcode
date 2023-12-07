8e29b5802072de61c75a9d0e38f758a8a17ea562
Ross Nelson <ross.nelson@gmail.com>
2023-12-06 18:52:37 -0600

    Add files for day 7

---
+++ advent2023/src/test/kotlin/Day07Test.kt
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day07Test {
+    private val input = arrayOf("")
+
+    @Test
+    fun partA() {
+        val actual = Day07().partA(input)
+        assertEquals(0, actual)
+    }
+
+    @Test
+    fun partB() {
+        val actual = Day07().partB(input)
+        assertEquals(0, actual)
+    }
+}
