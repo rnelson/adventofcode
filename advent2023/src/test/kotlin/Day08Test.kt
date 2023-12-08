031dd3b76d7668d5371e8f96e78cc723beb68cbe
Ross Nelson <ross.nelson@gmail.com>
2023-12-08 11:49:15 -0600

    8b safety

--- advent2023/src/test/kotlin/Day08Test.kt
+++ advent2023/src/test/kotlin/Day08Test.kt
@@ -2,7 +2,9 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class Day08Test {
-    private val input = arrayOf("RL",
+    @Test
+    fun partA() {
+        val input = arrayOf("RL",
             "",
             "AAA = (BBB, CCC)",
             "BBB = (DDD, EEE)",
@@ -11,16 +13,22 @@ class Day08Test {
             "EEE = (EEE, EEE)",
             "GGG = (GGG, GGG)",
             "ZZZ = (ZZZ, ZZZ)")
-
-    @Test
-    fun partA() {
         val actual = Day08().partA(input)
         assertEquals(2, actual)
     }
 
     @Test
-    fun partB() {
+    fun partB() {val input = arrayOf("LR",
+            "",
+            "11A = (11B, XXX)",
+            "11B = (XXX, 11Z)",
+            "11Z = (11B, XXX)",
+            "22A = (22B, XXX)",
+            "22B = (22C, 22C)",
+            "22C = (22Z, 22Z)",
+            "22Z = (22B, 22B)",
+            "XXX = (XXX, XXX)")
         val actual = Day08().partB(input)
-        assertEquals(0, actual)
+        assertEquals(6L, actual)
     }
 }
