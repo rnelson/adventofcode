aa34c4da4e08f28f5a75f1cd30f74f5eb51b8b2f
Ross Nelson <ross.nelson@gmail.com>
2023-12-08 11:52:51 -0600

    d9 files

---
+++ advent2023/src/main/kotlin/Day09.kt
@@ -0,0 +1,9 @@
+class Day09 : Day(9) {
+    override fun partA(input: Array<String>): Any {
+        return 0
+    }
+
+    override fun partB(input: Array<String>): Any {
+        return 0
+    }
+}
