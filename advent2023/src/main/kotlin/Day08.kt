d4c1d486188b75d2ca8b7ddba185b971478b7034
Ross Nelson <ross.nelson@gmail.com>
2023-12-08 11:22:14 -0600

    D'oh...we're still doing AAA and ZZZ lol

--- advent2023/src/main/kotlin/Day08.kt
+++ advent2023/src/main/kotlin/Day08.kt
@@ -11,55 +11,52 @@ class Day08 : Day(8) {
 
     private fun solveA(input: Array<String>): Int {
         val data = parse(input)
-        val steps = data.first.toCharArray()
+        val steps = data.steps.toCharArray()
 
-        var currentNode = data.second[0]
+        var currentNode = "AAA" //data.first
         var currentStep = -1
         var count = 0
 
-        while (currentNode.label != data.second[data.second.size - 1].label) {
-            // Make sure we don't go past the end of the instructions
-            currentStep++;
+        //while (currentNode != data.last) {
+        while (currentNode != "ZZZ") {
+            count += 1
+            currentStep += 1
             if (currentStep >= steps.size) {
                 currentStep = 0
             }
 
-            // Increment
-            val step = steps[currentStep]
-            count += 1
-
-            //print("Instructed to go $step from ${current.label}, headed to ")
-
-            currentNode = when (step) {
-                'L' -> findNode(currentNode.left, data)
-                'R' -> findNode(currentNode.right, data)
-                else -> throw Error("unexpected step '$step'")
+            val direction = steps[currentStep]
+            currentNode = when (direction) {
+                'L' -> data.nodes[currentNode]!!.first
+                'R' -> data.nodes[currentNode]!!.second
+                else -> throw Error("unexpected step '$direction'")
             }
-
-            //println("${current.label}.")
         }
 
         return count
     }
 
-    private fun findNode(label: String, data: Pair<String, Array<Node>>): Node {
-        return data.second.first { it.label == label }
-    }
-
-    private fun parse(input: Array<String>): Pair<String, Array<Node>> {
-        val nodes = mutableListOf<Node>()
+    private fun parse(input: Array<String>): Input {
+        val nodes = mutableMapOf<String, Pair<String, String>>()
         val steps = input[0]
         val regex = """(\w+) = \((\w+), (\w+)\)""".toRegex()
+        var first = ""; var last = ""
 
         for (i in input.indices) {
             if (i < 2) continue
 
             val (n, l, r) = regex.find(input[i])!!.destructured
-            nodes.add(Node(n, l, r))
+            nodes[n] = Pair(l, r)
+
+            if (i == 2) first = n
+            last = n
         }
 
-        return Pair(steps, nodes.toTypedArray())
+        return Input(steps, nodes, first, last)
     }
 
-    data class Node(val label: String, val left: String, val right: String)
+    data class Input(val steps: String,
+        val nodes: Map<String, Pair<String, String>>,
+        val first: String,
+        val last: String)
 }
