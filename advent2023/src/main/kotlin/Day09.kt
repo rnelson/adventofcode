5a8e34ed8fc775c6f029e414973d265850d8e90a
Ross Nelson <ross.nelson@gmail.com>
2023-12-09 15:10:32 -0600

    I don't like day 9

--- advent2023/src/main/kotlin/Day09.kt
+++ advent2023/src/main/kotlin/Day09.kt
@@ -1,9 +1,29 @@
 class Day09 : Day(9) {
     override fun partA(input: Array<String>): Any {
+        val seq = input.map { l ->
+            generateSequence(l.getIntegers().toList()) {
+                    s -> s.windowed(2) { (a, b) -> b - a }
+            /*}.takeWhile {
+                it.none { n -> n != 0 }
+            }.toList()*/
+                .take(1) }
+        }
+        //val sum = seq.sumOf { s -> s.sumOf { it.last() } }
+
+        val a = seq.take(10)
+
+        seq.forEach { println(it) }
+
         return 0
     }
 
     override fun partB(input: Array<String>): Any {
         return 0
+    }
+
+    private fun parse(input: Array<String>): Array<Long> {
+        val result = mutableListOf<Long>()
+        input.forEach { result.addAll(it.getLongs()) }
+        return result.toTypedArray()
     }
 }
--- advent2023/src/test/kotlin/Day09Test.kt
+++ advent2023/src/test/kotlin/Day09Test.kt
@@ -2,12 +2,14 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class Day09Test {
-    private val input = arrayOf("")
+    private val input = arrayOf("0 3 6 9 12 15",
+            "1 3 6 10 15 21",
+            "10 13 16 21 30 45")
 
     @Test
     fun partA() {
         val actual = Day09().partA(input)
-        assertEquals(0, actual)
+        assertEquals(114, actual)
     }
 
     @Test
