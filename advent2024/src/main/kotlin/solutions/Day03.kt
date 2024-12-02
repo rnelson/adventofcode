468505e22396fbc20cf7e58b8628969f7c7b3b6a
Ross Nelson <ross.nelson@gmail.com>
2024-12-02 16:19:26 -0600

    Add structure for day 3

---
+++ advent2024/src/main/kotlin/solutions/Day03.kt
@@ -0,0 +1,15 @@
+package net.libexec.adventofcode.solutions
+
+import net.libexec.adventofcode.Day
+
+class Day03(loadData: Boolean = true) : Day("03", loadData) {
+    override fun solveA(): String {
+        //val values = FileHelper.asDoubles(filename)
+        TODO("Not yet implemented")
+    }
+
+    override fun solveB(): String {
+        //val values = FileHelper.asDoubles(filename)
+        TODO("Not yet implemented")
+    }
+}
