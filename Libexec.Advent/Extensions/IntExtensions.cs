a8e6ef980140e6c20322de9e29c5c304bcf48e1f
Ross Nelson <ross.nelson@gmail.com>
2024-12-03 12:04:14 -0600

    Add doc comments to the extension classes

--- Libexec.Advent/Extensions/IntExtensions.cs
+++ Libexec.Advent/Extensions/IntExtensions.cs
@@ -2,12 +2,32 @@
 
 namespace Libexec.Advent.Extensions;
 
+/// <summary>
+/// Extensions to <see cref="int"/>.
+/// </summary>
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 [SuppressMessage("ReSharper", "UnusedType.Global")]
 [SuppressMessage("ReSharper", "UnusedMember.Global")]
 public static class IntExtensions
 {
-    public static int Increment(this int value, int minimum, int maximum) => value + 1 <= maximum ? value + 1 : minimum;
+    /// <summary>
+    /// Increments this integer. If the new value exceeds <paramref name="maximum"/>, it will
+    /// circle around and become <paramref name="minimum"/>.
+    /// </summary>
+    /// <param name="value">The integer.</param>
+    /// <param name="minimum">The minimum value for the integer.</param>
+    /// <param name="maximum">The maximum value for the integer.</param>
+    /// <returns>The incremented integer.</returns>
+    public static int CircularIncrement(this int value, int minimum, int maximum) => value + 1 <= maximum ? value + 1 : minimum;
 	
-    public static int Decrement(this int value, int minimum, int maximum) => value - 1 >= minimum ? value - 1 : maximum;
+    
+    /// <summary>
+    /// Decrements this integer. If the new value is smaller than <paramref name="minimum"/>, it will
+    /// circle around and become <paramref name="maximum"/>.
+    /// </summary>
+    /// <param name="value">The integer.</param>
+    /// <param name="minimum">The minimum value for the integer.</param>
+    /// <param name="maximum">The maximum value for the integer.</param>
+    /// <returns>The decremented integer.</returns>
+    public static int CircularDecrement(this int value, int minimum, int maximum) => value - 1 >= minimum ? value - 1 : maximum;
 }
--- Libexec.Advent/Extensions/StringExtensions.cs
+++ Libexec.Advent/Extensions/StringExtensions.cs
@@ -4,26 +4,53 @@ using System.Numerics;
 
 namespace Libexec.Advent.Extensions;
 
+/// <summary>
+/// Extensions to <see cref="string"/>.
+/// </summary>
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 [SuppressMessage("ReSharper", "UnusedType.Global")]
 [SuppressMessage("ReSharper", "UnusedMember.Global")]
 public static class StringExtensions
 {
+    /// <summary>
+    /// Splits the string in half.
+    /// </summary>
+    /// <param name="s">The string.</param>
+    /// <returns>A tuple containing each half of the string.</returns>
     public static (string, string) Halve(this string s) => (s[..(s.Length / 2)], s[^(s.Length / 2)..]);
     
-    public static string Intersection(this string s1, string s2)
+    /// <summary>
+    /// Determines the common characters between this string and another.
+    /// </summary>
+    /// <param name="s">The string.</param>
+    /// <param name="other">The other string.</param>
+    /// <returns>A string containing the common characters between the two strings.</returns>
+    public static string Intersection(this string s, string other)
     {
-        var s1Chars = s1.Distinct().Select(c => c.ToString()).ToArray();
-        var s2Chars = s2.Distinct().Select(c => c.ToString()).ToArray();
+        var s1Chars = s.Distinct().Select(c => c.ToString()).ToArray();
+        var s2Chars = other.Distinct().Select(c => c.ToString()).ToArray();
 
         var intersection = s1Chars.Intersect(s2Chars, StringComparer.Ordinal);
         return string.Join(string.Empty, intersection);
     }
 
+    /// <summary>
+    /// Parses many <typeparamref name="T"/>s out of this string.
+    /// </summary>
+    /// <param name="s">The string.</param>
+    /// <param name="delimiter">The delimiter (by default a space) between the <typeparamref name="T"/>s.</param>
+    /// <typeparam name="T">The <see cref="INumber{TSelf}"/> to parse out of the string.</typeparam>
+    /// <returns>An <see cref="IEnumerable{T}"/> of <typeparamref name="T"/>s.</returns>
     public static IEnumerable<T> ParseMany<T>(this string s, char delimiter = ' ') where T : INumber<T> => s
         .Split(delimiter, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
         .Select(b => T.Parse(b, CultureInfo.CurrentCulture));
 
+    /// <summary>
+    /// Returns substrings of the string of up to <paramref name="size"/> characters.
+    /// </summary>
+    /// <param name="s">The string.</param>
+    /// <param name="size">Maximum window size.</param>
+    /// <returns>An <see cref="IEnumerable{T}"/> of substrings.</returns>
     public static IEnumerable<string> SlidingWindows(this string s, int size)
     {
         if (s.Length < size)
