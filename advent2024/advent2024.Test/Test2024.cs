adf6fce32cfaac1927a58dd6afb8a73c111d1d54
Ross Nelson <ross.nelson@gmail.com>
2024-12-03 11:18:43 -0600

    Move static methods to a partial class

--- advent2024/advent2024.Test/Test2024.cs
+++ advent2024/advent2024.Test/Test2024.cs
@@ -5,18 +5,19 @@ using Xunit.Abstractions;
 namespace advent2024.Test;
 
 /// <summary>
-/// 2024 test file
+/// 2024 test file.
 /// </summary>
-public class Test2024(ITestOutputHelper testOutputHelper)
+/// <param name="testOutputHelper"><see cref="ITestOutputHelper"/> to provide output.</param>
+public partial class Test2024(ITestOutputHelper testOutputHelper)
 {
     /// <summary>
-    /// Run all tests
+    /// Run all tests for <see cref="dayType"/>.
     /// </summary>
-    /// <param name="dayType">the <see cref="Type"/> for the <see cref="Day"/> to test</param>
-    /// <param name="expectedA">expected answer for part A</param>
-    /// <param name="expectedB">expected answer for part B</param>
-    /// <param name="isTest"><c>true</c> to load test data, <c>false</c> to load real data</param>
-    /// <param name="twoPartTest"><c>true</c> for separate "a" and "b" test input files, otherwise <c>false</c></param>
+    /// <param name="dayType">the <see cref="Type"/> for the <see cref="Day"/> to test.</param>
+    /// <param name="expectedA">expected answer for part A.</param>
+    /// <param name="expectedB">expected answer for part B.</param>
+    /// <param name="isTest"><c>true</c> to load test data, <c>false</c> to load real data.</param>
+    /// <param name="twoPartTest"><c>true</c> for separate "a" and "b" test input files, otherwise <c>false</c>.</param>
     [Theory]
     [InlineData(typeof(Day01), "11", "31", true)]
     [InlineData(typeof(Day01), "2378066", "18934359", false)]
@@ -44,18 +45,4 @@ public class Test2024(ITestOutputHelper testOutputHelper)
         Assert.Equal(expectedA, actualA);
         Assert.Equal(expectedB, actualB);
     }
-
-    private static Day GetDay(Type dayType, bool isTest, string fileSuffix = "")
-    {
-        if (Activator.CreateInstance(dayType, isTest, fileSuffix) is not Day day)
-            throw new Exception($"unable to instantiate type {dayType.FullName}");
-
-        return day;
-    }
-    
-    private static string SolveA(Type dayType, bool isTest, string fileSuffix = "a") =>
-        GetDay(dayType, isTest, fileSuffix).PartA().ToString()!;
-    
-    private static string SolveB(Type dayType, bool isTest, string fileSuffix = "b") =>
-        GetDay(dayType, isTest, fileSuffix).PartB().ToString()!;
 }
