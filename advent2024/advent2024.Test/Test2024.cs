64de691b2cb84d9a252bfd89eb5d3097e176832b
Ross Nelson <ross.nelson@gmail.com>
2024-12-15 10:06:51 -0600

    Add placeholder for day 15 results

--- advent2024/advent2024.Test/Test2024.cs
+++ advent2024/advent2024.Test/Test2024.cs
@@ -47,6 +47,8 @@ public partial class Test2024(ITestOutputHelper testOutputHelper)
     [InlineData(typeof(Day13), "33921", "82261957837868")]
     //[InlineData(typeof(Day14), "12", "", true)]
     //[InlineData(typeof(Day14), "", "")]
+    //[InlineData(typeof(Day15), "10092", "", true)]
+    //[InlineData(typeof(Day15), "", "")]
     public void RunTests(Type dayType, string expectedA, string expectedB, bool isTest = false, bool twoPartTest = false)
     {
         string actualA, actualB;
